c66c959579ee99f0374cd1b34cdc12df21441316
usaamatahir <usamatahir8330@gmail.com>
2020-11-28 10:54:14 +0500

    Added config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   plugins: [
-//     {
-//       resolve: "gatsby-source-graphql",
-//       options: {
-//         // This type will contain remote schema Query type
-//         typeName: "Lollies",
-//         // This is the field under which it's accessible
-//         fieldName: "LOLLIES",
-//         // URL to query from
-//         url:
-//           "https://virtuallolly-with-gatsby.netlify.app/.netlify/functions/LollyPop",
-//       },
-//     },
-//   ],
-// };
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "Lollies",
+        // This is the field under which it's accessible
+        fieldName: "LOLLIES",
+        // URL to query from
+        url:
+          "https://virtual-loly-with-gatsby.netlify.app/.netlify/functions/LollyPop",
+      },
+    },
+  ],
+};
