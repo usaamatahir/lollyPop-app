af518f17ce32917d0e11cd0b8803cadb6068a8dd
usaamatahir <usamatahir8330@gmail.com>
2020-11-28 19:47:02 +0500

    Updated

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,22 +5,20 @@ exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(`
     query MyQuery {
       Lollies {
-        getLollies {
-          id
-          colorTop
-          colorMiddle
-          colorBottom
-          recipient
-          message
-          sender
-          lollyPath
-        }
+        id
+        colorTop
+        colorMiddle
+        colorBottom
+        recipient
+        message
+        sender
+        lollyPath
       }
     }
   `);
 
   console.log(result);
-  result.data.Lollies.getLollies.map((data) => {
+  result.data.getLollies.map((data) => {
     createPage({
       path: `${data.lollyPath}`,
       component: path.resolve("./src/Template/Template.tsx"),
