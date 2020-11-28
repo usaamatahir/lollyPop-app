818309d2ccf83705befd05d32edc6b3a8d3641aa
usaamatahir <usamatahir8330@gmail.com>
2020-11-28 20:03:20 +0500

    Remove Some COde

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // This type will contain remote schema Query type
-        typeName: `Lollies`,
-        // This is the field under which it's accessible
-        fieldName: `Lollies`,
-        // URL to query from
-        url: `https://virtual-loly-with-gatsby.netlify.app/.netlify/functions/LollyPop`,
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     // This type will contain remote schema Query type
+    //     typeName: `Lollies`,
+    //     // This is the field under which it's accessible
+    //     fieldName: `Lollies`,
+    //     // URL to query from
+    //     url: `https://virtual-loly-with-gatsby.netlify.app/.netlify/functions/LollyPop`,
+    //   },
+    // },
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const result = await graphql(`
     query MyQuery {
-      Lollies {
+      getLollies {
         id
         colorTop
         colorMiddle
@@ -18,7 +18,7 @@ exports.createPages = async ({ graphql, actions }) => {
   `);
 
   console.log(result);
-  result.data.Lollies.getLollies.map((data) => {
+  result.data.getLollies.map((data) => {
     createPage({
       path: `${data.lollyPath}`,
       component: path.resolve("./src/Template/Template.tsx"),
