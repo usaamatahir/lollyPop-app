0a772840fea06d494203d32278e173c1e717e9e2
usaamatahir <usamatahir8330@gmail.com>
2020-11-28 11:25:51 +0500

    Removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,17 @@
-module.exports = {
-  plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // This type will contain remote schema Query type
-        typeName: `Lollies`,
-        // This is the field under which it's accessible
-        fieldName: `LOLLIES`,
-        // URL to query from
-        url: `https://virtual-loly-with-gatsby.netlify.app/.netlify/functions/LollyPop`,
-        refetchInterval: 60,
-      },
-    },
-  ],
-};
+// module.exports = {
+//   plugins: [
+//     {
+//       resolve: "gatsby-source-graphql",
+//       options: {
+//         // This type will contain remote schema Query type
+//         typeName: `Lollies`,
+//         // This is the field under which it's accessible
+//         fieldName: `LOLLIES`,
+//         // URL to query from
+//         url: `https://virtual-loly-with-gatsby.netlify.app/.netlify/functions/LollyPop`,
+
+//         refetchInterval: 60,
+//       },
+//     },
+//   ],
+// };
